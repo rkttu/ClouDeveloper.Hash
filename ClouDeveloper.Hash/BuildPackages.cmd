55f194d414131445ede0af26fd64f79d287f09d1
rkttu <rkttu@xinics.com>
2015-04-24 16:51:39 +0900

    add nuget publish command to BuildPackages.cmd script.

--- ClouDeveloper.Hash/BuildPackages.cmd
+++ ClouDeveloper.Hash/BuildPackages.cmd
@@ -7,7 +7,7 @@ if not exist RedistPackages\libs mkdir RedistPackages\libs
 set projectname=ClouDeveloper.Hash
 .nuget\nuget.exe pack %projectname%\%projectname%.csproj -Build -Symbols -IncludeReferencedProjects -OutputDirectory "RedistPackages\libs" -Properties configuration=Release
 
-start RedistPackages
+.nuget\NuGet.exe push RedistPackages\libs\ClouDeveloper.Hash.*.nupkg
 
 popd
 @echo on
